36b2e616074bc07f7e4f78a21899f0e4ffe76396
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-06-19 19:41:44 -0300

    fix: 🐛 use env variables and exports

--- src/firebase/main.js
+++ src/firebase/main.js
@@ -1,12 +1,16 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: 'cinehub-59a36.firebaseapp.com',
   projectId: 'cinehub-59a36',
   storageBucket: 'cinehub-59a36.appspot.com',
   messagingSenderId: '358645305248',
-  appId: process.env.VITE_FIREBASE_APP_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
