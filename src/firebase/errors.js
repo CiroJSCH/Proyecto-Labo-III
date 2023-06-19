c00d79ab8ef7ce886def19afbda6f0d216d27b27
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-06-19 19:40:56 -0300

    feat: ✨ firebase map errors

---
+++ src/firebase/errors.js
@@ -0,0 +1,7 @@
+const errors = {
+  'auth/invalid-email': 'Email is invalid',
+  'auth/email-already-in-use': 'Email is already in use',
+  'auth/weak-password': 'Password is too weak',
+};
+
+export default errors;
