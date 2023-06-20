4468dd5c66dfb91613add2f63c94493145bd73ab
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-06-20 19:30:22 -0300

    feat: ✨ login errors handle

--- src/scripts/login/main.js
+++ src/scripts/login/main.js
@@ -1,6 +1,11 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase/main.js';
 import changePasswordVisibility from '../common/changePasswordVisibility.js';
+import errors from '../../firebase/errors.js';
+
+const loginError = document.getElementById('loginError');
+const emailContainer = document.getElementById('emailContainer');
+const passwordContainer = document.getElementById('passwordContainer');
 
 const passwordToggle = document.getElementById('password-toggle');
 passwordToggle.addEventListener('click', () => {
@@ -14,11 +19,39 @@ signInForm.addEventListener('submit', async (e) => {
   const email = signInForm.email.value;
   const password = signInForm.password.value;
 
+  if (email.trim().length === 0) {
+    emailContainer.classList.replace('border-l-accent', 'border-l-red-500');
+    loginError.innerHTML = 'Email required';
+    return;
+  }
+
+  if (password.trim().length === 0) {
+    passwordContainer.classList.replace('border-l-accent', 'border-l-red-500');
+    loginError.innerHTML = 'Password required';
+    return;
+  }
+
+  passwordContainer.classList.replace('border-l-red-500', 'border-l-accent');
+  emailContainer.classList.replace('border-l-red-500', 'border-l-accent');
+
   try {
-    const credentials = await signInWithEmailAndPassword(auth, email, password);
-    console.log(credentials);
+    await signInWithEmailAndPassword(auth, email, password);
     window.location.href = `${window.location.origin}/src/pages/home.html`;
   } catch (error) {
-    console.log(error.code);
+    loginError.innerHTML = errors[error.code];
+
+    if (errors[error.code].includes('Password')) {
+      passwordContainer.classList.replace(
+        'border-l-accent',
+        'border-l-red-500',
+      );
+      emailContainer.classList.replace('border-l-red-500', 'border-l-accent');
+    } else {
+      emailContainer.classList.replace('border-l-accent', 'border-l-red-500');
+      passwordContainer.classList.replace(
+        'border-l-red-500',
+        'border-l-accent',
+      );
+    }
   }
 });
