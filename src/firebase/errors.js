cf4331bc2e8a285c88be96d29eb8c2f18e474448
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-06-19 23:51:21 -0300

    ci: 👷 added login errors

--- src/firebase/errors.js
+++ src/firebase/errors.js
@@ -2,6 +2,8 @@ const errors = {
   'auth/invalid-email': 'Email is invalid',
   'auth/email-already-in-use': 'Email is already in use',
   'auth/weak-password': 'Password is too weak',
+  'auth/wrong-password': 'Password is incorrect',
+  'auth/user-not-found': 'User not found',
 };
 
 export default errors;
