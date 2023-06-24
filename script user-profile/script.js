4724bf15b4e7a5d3482b77d57a11c5a68d898ef8
Agusss33 <agustinnatali7@gmail.com>
2023-06-24 16:21:17 -0300

    feat: hice responsive y falta agregar opciones

--- script user-profile/script.js
+++ script user-profile/script.js
@@ -1,5 +1,5 @@
 const carousel = document.querySelector(".carousel");
-firstImg = carousel.querySelectorAll("img")[0];
+firstImg = carousel.querySelectorAll("img.carousel")[0];
 
 let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
 
@@ -57,5 +57,30 @@ carousel.addEventListener("touchmove", dragging);
 document.addEventListener("mouseup", dragStop);
 carousel.addEventListener("touchend", dragStop);
 
-/////////////////////////////////////////////////////////////////////////////////
 
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      "@0.00": {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      "@0.75": {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      "@1.00": {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      "@1.50": {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
