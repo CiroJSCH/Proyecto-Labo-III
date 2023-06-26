e9a2b81a5c304d1c151123deff43541056d50dfa
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-06-26 12:20:38 -0300

    ci: 👷 added vite config

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { resolve } from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'src/pages/login.html'),
+        register: resolve(__dirname, 'src/pages/register.html'),
+        main: resolve(__dirname, 'src/pages/main.html'),
+        series: resolve(__dirname, 'src/pages/series.html'),
+        vistaGuest: resolve(__dirname, 'src/pages/vista-guest.html'),
+        seeMoreMovies: resolve(__dirname, 'src/pages/see-more-movies.html'),
+        movieDetail: resolve(__dirname, 'src/pages/movieDetail.html'),
+        userSaves: resolve(__dirname, 'src/pages/user-saves.html'),
+        verMas: resolve(__dirname, 'src/pages/ver-mas.html'),
+      },
+    },
+  },
+});
