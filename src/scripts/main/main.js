db2d544b26d8a2346859fcccf1edb7af528e4a98
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-06-26 08:48:13 -0300

    ci: 👷 separated sliders in other file

--- src/scripts/main/main.js
+++ src/scripts/main/main.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-new */
-/* eslint-disable no-undef */
-
 import {
   getMovies,
   getPopularSeries,
@@ -194,80 +191,4 @@ getUpcomingMovies().then((response) => {
     tempUpcomingContainer.appendChild(card);
   });
   upcomingContainer.appendChild(tempUpcomingContainer);
-});
-
-new Swiper('#swiper-toprated', {
-  slidesPerView: 1,
-  centeredSlides: true,
-  spaceBetween: 24,
-  lazyLoading: true,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: true,
-  },
-  keyboard: {
-    enabled: true,
-  },
-  breakpoints: {
-    620: {
-      slidesPerView: 2,
-      centeredSlides: false,
-    },
-    1440: {
-      slidesPerView: 3,
-      centeredSlides: false,
-    },
-    1640: {
-      slidesPerView: 4,
-      centeredSlides: false,
-    },
-  },
-});
-
-new Swiper('#swiper-popular', {
-  slidesPerView: 1,
-  centeredSlides: true,
-  spaceBetween: 24,
-  lazyLoading: true,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: true,
-  },
-  keyboard: {
-    enabled: true,
-  },
-});
-
-new Swiper('#swiper-series', {
-  slidesPerView: 1,
-  centeredSlides: true,
-  spaceBetween: 24,
-  lazyLoading: true,
-  loop: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: true,
-  },
-  keyboard: {
-    enabled: true,
-  },
-  breakpoints: {
-    620: {
-      slidesPerView: 2,
-    },
-    940: {
-      slidesPerView: 4,
-      centeredSlides: false,
-    },
-    1200: {
-      slidesPerView: 5,
-      centeredSlides: false,
-    },
-    1440: {
-      slidesPerView: 6,
-      centeredSlides: false,
-    },
-  },
 });
