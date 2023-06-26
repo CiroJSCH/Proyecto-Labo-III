c3b242a4dcf8894e3111b7097abee7dd2360dd44
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-06-26 13:23:23 -0300

    fix: 🐛 removed dist

--- dist/assets/getMovie-d963b739.js
+++
@@ -1,1 +0,0 @@
-const t=async e=>(await fetch(`https://api.themoviedb.org/3/movie/${e}?api_key=7e166e717fba198f23734103f094beb2`)).json(),o=async e=>(await fetch(`https://api.themoviedb.org/3/movie/${e}/reviews?api_key=7e166e717fba198f23734103f094beb2`)).json();export{o as a,t as g};
