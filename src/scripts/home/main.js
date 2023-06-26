d6cbaf70bf0ac10b86b670edcdc648a72e8f0e60
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-06-26 19:18:44 -0300

    fix: 🐛 ayuda dios 3

--- src/scripts/home/main.js
+++
@@ -1,9 +0,0 @@
-import { checkAuthState } from '../common/authUser.js';
-
-checkAuthState((user) => {
-  if (user) {
-    console.log('Usuario autenticado:', user);
-  } else {
-    console.log('Ningún usuario autenticado');
-  }
-});
